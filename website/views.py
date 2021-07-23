f334335fc82232c5c8191a4982f73cd1699cd22b
TinkInc <thinktinkinc@gmail.com>
2021-07-23 13:22:35 -0400

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+# Create your views here.
+#{} = content dictionary pass from backend to front end
+
+def home(request):
+	return render(request, 'home.html',{})
+
+def contact(request):
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+
+		#send email
+		send_mail(
+			  message_name 
+		    , message 
+			, message_email
+			, ['rm17tink@gmail.com'] 
+			,fail_silently=False
+			)
+
+		return render(request, 'contact.html',{'message_name': message_name})
+	
+	else:
+		
+		return render(request, 'contact.html',{})
